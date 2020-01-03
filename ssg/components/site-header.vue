34b69b5f10d4e618dde231a47c0bafd2f7b7c4d3
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-01-03 19:50:00 +1000

    Add styles for site header and tweak font sizes

--- ssg/components/site-header.vue
+++ ssg/components/site-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="site-header">
     <text-logo />
     <nav>
       <ul>
@@ -26,4 +26,40 @@
   }
 </script>
 
-<style></style>
+<style lang="scss">
+  @use '~assets/scss/abstracts/variables/colours';
+  @use '~assets/scss/abstracts/variables/text';
+  @use '~assets/scss/abstracts/variables/grid';
+
+  .site-header {
+    margin-top: 2rem;
+    border-bottom: 1px solid colours.$black;
+
+    svg {
+      width: 30rem;
+      margin: 0 auto;
+      fill: colours.$black;
+    }
+
+    nav {
+      font-family: text.$font-secondary;
+      line-height: 1rem;
+      letter-spacing: .02em;
+      margin: grid.$gutter;
+    }
+
+    ul {
+      display: flex;
+      justify-content: center;
+
+      > li {
+        &:not(:last-child)::after {
+          content: '|';
+          color: colours.$grey;
+          display: inline-block;
+          margin: 0 .5rem;
+        }
+      }
+    }
+  }
+</style>
--- ssg/layouts/default.vue
+++ ssg/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <site-header />
     <nuxt />
   </div>
